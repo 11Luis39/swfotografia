29a09bf65c29eeef702f917436201ef24fc47b27
TonyTorres <soloalaaalaz@gmail.com>
2023-11-29 11:07:09 -0400

    mi tarea

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose, { connect } from 'mongoose';
 
 export const connectDB = async () => { 
     try {
-        await mongoose.connect("mongodb://localhost/swfoto");
+        await mongoose.connect("mongodb+srv://luis:77807205Luis@swfotos.hcgp7k3.mongodb.net/?retryWrites=true&w=majority");
         console.log("DB is connects");
     } catch (error) {
         console.log(error);
